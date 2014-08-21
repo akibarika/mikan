7dd45716a8d84552f415a797d831aa538212e53a
akibarika <lxclxc89816@gmail.com>
2014-08-21 16:36:56 +1200

    updating

--- single.php
+++ single.php
@@ -6,13 +6,17 @@
  * Time: 4:11 下午
  */
 get_header(); ?>
-
+        <?php while ( have_posts() ) : the_post(); ?>
         <article class="post-content post">
             <div class="post-heading">
                 <h1><?php the_title(); ?></h1>
+                <div class="post-date">
+                    由<strong> <?php echo get_the_author_link(); ?> </strong>发布于 <?php the_time('Y/m/d') ?>
+                </div>
             </div>
 
         </article>
+        <?php endwhile; ?>
     </div>
 </section>
 
