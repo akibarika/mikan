90f4b9f681fd87f318446c0064eb1563f7642788
akibarika <lxclxc89816@gmail.com>
2014-08-27 15:19:41 +1200

    updating
    post detail style
    new icon support

--- footer.php
+++ footer.php
@@ -1,14 +1,13 @@
-<footer id="footer">
+<footer id="footer" class="cf">
     <div class="container">
         <div class="wrap-footer">
             <?php if ( is_single() ){ ?>
-            <span class="icon-dir to-left"></span>
-            <span class="icon-up"></span>
-            <span class="icon-dir to-right"></span>
+            <span class="icon-arrow-up"></span>
             <?php } else { ?>
-             <div class="more">
-            <?php echo get_next_posts_link('<span title="显示更多" class="icon-refresh"></span>'); ?></div>
-            <span class="icon-up"></span>
+            <div class="more">
+                <?php echo get_next_posts_link('<span title="显示更多" class="icon-refresh"></span>'); ?>
+            </div>
+            <span class="icon-arrow-up"></span>
             <?php } ?>
             <p class="cp">Made with <span class="heart"><3</span> in Theme <span class="mikan">Mikan</span></p>
         </div>
--- single.php
+++ single.php
@@ -14,7 +14,7 @@ get_header(); ?>
                     由<strong> <?php echo get_the_author_link(); ?> </strong>发布在 <?php the_category(' '); ?>
                 </div>
             </div>
-            <div class="post-detail cf">
+            <div class="post-detail">
                 <?php the_content(); ?>
                 <div class="copyright">
                     <p>Transshipment is forbidden</p>
@@ -24,8 +24,20 @@ get_header(); ?>
             <div class="single-tags">
                 <?php the_tags(' ',' '); ?>
             </div>
+            <div class="single-share">
+                <a href="http://service.weibo.com/share/share.php?url=<?php the_permalink() ?>&appkey=&title=<?php the_title(); ?>&pic=&ralateUid=&language=zh_cn" onclick="window.open(this.href, 'weibo-share', 'width=550,height=235');return false;"><span class="icon-sina-weibo"></span></a>
+                <a href="http://twitter.com/share?text=<?php the_title(); ?>&url=<?php the_permalink() ?>" onclick="window.open(this.href, 'twitter-share', 'width=550,height=235');return false;"><span class="icon-twitter"></span></a>
+                <a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink() ?>" onclick="window.open(this.href, 'facebook-share', 'width=550,height=235');return false;"><span class="icon-facebook"></span></a>
+                <a href="https://plus.google.com/share?url=<?php the_permalink() ?>" onclick="window.open(this.href, 'google-plus-share', 'width=550,height=235');return false;"><span class="icon-googleplus"></span></a>
+            </div>
+            <div class="single-nav">
+                <?php previous_post_link('%link','<span title="上一篇" class="icon-arrow-left"></span>'); ?>
+
+                <?php next_post_link('%link','<span title="下一篇" class="icon-arrow-right"></span>'); ?>
+            </div>
 
         </article>
+
         <?php endwhile; ?>
     </div>
 </section>
