f3139d7898666eec6fd88513a46ea8f4a6ac482a
akibarika <lxclxc89816@gmail.com>
2014-08-28 17:03:16 +1200

    update comment

--- comments.php
+++ comments.php
@@ -9,27 +9,71 @@ if ( post_password_required() ) : ?>
     <?php _e( 'Enter your password to view comments.' ); ?>
     <?php return; endif; ?>
 <div id="comments">
-    <div class="commentscount">
-        <h3><?php comments_number( '0', '1', '%' ); ?> Comments</h3>
-    </div>
     <div class="comt">
-<?php if ( have_comments() ) : ?>
-    <ol class="commentlist">
-        <?php wp_list_comments('type=comment&callback=mytheme_comment&max_depth=10000'); ?>
-    </ol>
-    <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
-        <div class="navigation cf">
-            <span class="left"><?php previous_comments_link( __( '&laquo; Older Comments' ) ); ?></span>
-            <span class="right"><?php next_comments_link( __( 'Newer Comments &raquo;' ) ); ?></span>
-        </div>
-    <?php endif; ?>
-<?php elseif ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
-    <p><?php _e( 'Comments are closed.' ); ?></p>
-<?php endif; ?>
-<?php
-$args =  array(
-    'comment_field'=> '<textarea id="comment" name="comment" cols="45" rows="8"></textarea>',
-    'label_submit'=> '确 认 提 交 / Ctrl + Enter',
-);
-comment_form($args);
-?>
+        <?php if ( have_comments() ) : ?>
+            <ol class="commentlist">
+                <?php wp_list_comments('type=comment&callback=mytheme_comment&max_depth=3'); ?>
+            </ol>
+            <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+                <div class="navigation">
+                    <div class="cf"><?php paginate_comments_links(); ?></div>
+                </div>
+            <?php endif; ?>
+        <?php endif; ?>
+        <?php if ('open' == $post->comment_status) : ?>
+            <div id="respond">
+                <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
+                <?php if ( $user_ID ) : ?>
+                    <div class="comtool cf">
+                        <?php
+                            if ( is_singular() )
+                                    wp_enqueue_script( "comment-reply" );
+                            cancel_comment_reply_link('取消回复')
+                        ?>
+                    </div>
+                    <div class="commenter">
+                    <div id="author_info" class="author_info cf" >
+                        <div class="user_logged cf">
+                            <span>欢迎回来</span> <?php	printf(__(' <a href="%1$s">%2$s</a> '), get_option('siteurl') . '/wp-admin/profile.php', $user_identity); ?>
+                            <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="<?php _e('Log out of this account'); ?>" class="user_logout"><?php _e('Log out'); ?></a>
+                        </div>
+                    </div>
+                    </div>
+                    <textarea name="comment" id="comment" rows="10" cols="100%" tabindex="4" onkeydown="if(event.ctrlKey&&event.keyCode==13){document.getElementById('submit').click();return false};"></textarea>
+                    <input type="submit" name="submit" id="submit" value="吐槽！" tabindex="5" >
+                    <?php comment_id_fields(); ?>
+                    <?php do_action('comment_form', $post->ID); ?>
+                    <input type="hidden" id="name_key" name="name_key" value="<?php echo wp_create_nonce(get_the_ID());  ?>">
+                    <?php else :?>
+                        <div class="cancel_comment_reply">
+                            <?php
+                                if ( is_singular() )
+                                    wp_enqueue_script( "comment-reply" );
+                                cancel_comment_reply_link('取消回复')
+                            ?>
+                        </div>
+                        <div class="commenter">
+                            <div id="author_info" class="author_info cf">
+                                <p class="cf">
+                                    <label class="author"><input placeholder="昵称" type="text" name="author" id="author" value="" tabindex="1"></label>
+                                    <label class="email"><input placeholder="电子邮箱" type="text" name="email" id="email" value="" tabindex="2"></label>
+                                </p>
+                                <p>
+                                <label class="url"><input placeholder="主页" type="text" name="url" id="url" value="" tabindex="3"></label>
+                                </p>
+                            </div>
+                        </div>
+                        <textarea name="comment" id="comment" rows="10" cols="100%" tabindex="4" onkeydown="if(event.ctrlKey&&event.keyCode==13){document.getElementById('submit').click();return false};"></textarea>
+                        <input type="submit" name="submit" id="submit" value="POST COMMENT" tabindex="5" >
+                        <?php comment_id_fields(); ?>
+                        <?php do_action('comment_form', $post->ID); ?>
+                        <input type="hidden" id="name_key" name="name_key" value="<?php echo wp_create_nonce(get_the_ID());  ?>">
+                    </div>
+
+                    <?php endif; ?>
+                </form>
+            <?php else : ?>
+                <div class="commentclosed"><?php _e( 'Comments are closed.' ); ?></div>
+            <?php endif; ?>
+    </div>
+</div>
--- functions.php
+++ functions.php
@@ -23,20 +23,26 @@ function mytheme_comment($comment, $args, $depth) {
 
     ?>
     <li <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>" <?php if( $depth > 1){ echo 'style="margin-left:35px;"';} ?>>
-        <div id="comment-<?php comment_ID(); ?>" class="comment-body">
-            <div class="comment-author"><?php echo get_avatar( $comment, $size = '34'); ?></div>
-            <div class="comment-head">
-                <span class="name"><?php printf(__('%s'), get_comment_author_link()) ?></span>
-                <div class="date"><?php printf(__('%1$s %2$s'), get_comment_date('F j, Y'),  get_comment_time('H:i:G')) ?> <?php if(!$parent_id = $comment->comment_parent) {printf('#%1$s', ++$commentcount);} ?></div>
-            </div>
-            <div class="comment-entry">
-                <div class="comment-text"><?php comment_text() ?>
-                    <?php if ($comment->comment_approved == '0') : ?>
-                        <em><?php _e('Your comment is awaiting moderation.') ?></em>
-                        <br />
-                    <?php endif; ?>
+        <div id="comment-<?php comment_ID(); ?>" class="comment-body cf">
+            <div class="comment-author">
+                <?php echo get_avatar( $comment, $size = '60'); ?>
+                <div class="comment-reply">
+                    <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth'], 'reply_text' => __('回复')))) ?>
                 </div>
-                <div class="comment-reply"><?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth'], 'reply_text' => __('回复')))) ?></div>
+            </div>
+            <div class="comment-wrapper">
+                <div class="comment-head">
+                    <span class="name"><?php comment_author_link(); ?>: </span>
+                    <span class="comment-text"><?php comment_text() ?>
+                        <?php if ($comment->comment_approved == '0') : ?>
+                            <em><?php _e('Your comment is awaiting moderation.') ?></em>
+                            <br />
+                        <?php endif; ?>
+                    </span>
+                </div>
+                <div class="comment-time">
+                    <div class="date"><?php echo get_comment_date('F j, Y') ?></div>
+                </div>
             </div>
         </div>
     </li>
